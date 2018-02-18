db790d5d072dd27c6c90a8fe0bf0aca57f5729fb
Xuwen <xuwenshen001@gmail.com>
2018-02-17 22:39:52 -0500

    preprocess2, fill na with 100

---
+++ pca.py
@@ -0,0 +1,6 @@
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest
